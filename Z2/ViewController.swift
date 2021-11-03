0d43b621247b1089f0bef09f1f72337bac49d466
zed <soksovanraksmey@gmail.com>
2021-11-03 10:45:11 +0700

    Initial Commit

---
+++ Z2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Z2
+//
+//  Created by zed on 11/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
